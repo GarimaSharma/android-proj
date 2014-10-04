c8171f5fe022378c41973039723d635229e1abfd
garimash <sharma_garima27@yahoo.com>
2014-10-04 12:46:48 +0530

    Garima added new log

--- src/utils/Logs.java
+++ src/utils/Logs.java
@@ -1,36 +1,45 @@
 package utils;
 
 import android.app.Activity;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 import android.util.Log;
+
+import java.io.*;
 
 public class Logs {
     String tag;
 
     public Logs(Activity activity) {
-        String apkName = getApkName(activity);
-        String apkNumber = "";
-        apkNumber = apkName.replace("/data/app/com.test.myapp-", "");
-        apkNumber = apkNumber.replace(".apk", "");
-        tag = " package name : " + activity.getPackageName() + " ,apk name : " + apkName + " ,apk-number : " +apkNumber;
+
+         tag = " package name : " + activity.getPackageName() + " build-number is " +getBuildNumber();
 
     }
 
-    private String getApkName(Activity activity) {
-        String sourceApk = null;
-        PackageManager pm = activity.getPackageManager();
+    private String getBuildNumber() {
+        String ret = null;
+        File fl = new File("/data/latestbuildnumber.txt");
+        FileInputStream fin = null;
         try {
-            ApplicationInfo ai = pm.getApplicationInfo("com.test.myapp", 0);
-            sourceApk = ai.publicSourceDir;
-        } catch (PackageManager.NameNotFoundException e) {
-            // TODO Auto-generated catch block
+            fin = new FileInputStream(fl);
+            ret = convertStreamToString(fin);
+            fin.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return sourceApk;
+        return ret;
     }
 
     public void log(String message){
         Log.i(tag, message);
+    }
+
+    public String convertStreamToString(InputStream is) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        reader.close();
+        return sb.toString();
     }
 }
